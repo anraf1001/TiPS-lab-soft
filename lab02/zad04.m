e5ab67aff2b08364a8faa241e78c5b54fe8d030c
Andrzej Rafalski <anraf1001@gmail.com>
2020-10-19 17:43:15 +0200

    PDF

--- lab02/zad04.m
+++ lab02/zad04.m
@@ -6,4 +6,16 @@ r = u + sigma * randn(1,n);
 u2 = mean(r);
 sigma2 = sqrt(var(r));
 
-histogram(r,10)
+bins_num = 10;
+hist = histogram(r, bins_num, 'Normalization', 'pdf');
+p = zeros(1, bins_num);
+
+for i=1:length(p)
+    p(i) = hist.Values(i);
+end
+
+first_point = hist.BinEdges(1) + hist.BinWidth / 2;
+last_point = first_point + hist.BinWidth * (bins_num - 1);
+hold on
+plot((first_point:hist.BinWidth:last_point),p)
+hold off
