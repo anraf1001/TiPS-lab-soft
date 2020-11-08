17d45da6d2573fcdb4a564012b1804635cbe1967
Andrzej Rafalski <anraf1001@gmail.com>
2020-11-08 12:45:27 +0100

    zad04 update

--- lab02/zad04.m
+++ lab02/zad04.m
@@ -9,12 +9,8 @@ sigma2 = sqrt(var(r));
 bins_num = 40;
 hist = histogram(r, bins_num, 'Normalization', 'pdf');
 
-first_point = hist.BinEdges(1) + hist.BinWidth / 2;
-last_point = first_point + hist.BinWidth * (bins_num - 1);
-
-midBins = (first_point:hist.BinWidth:last_point);
-pn = normpdf(midBins, u2, sigma2);
+pn = normpdf(hist.BinEdges(), u2, sigma2);
 
 hold on
-plot(midBins, pn)
+plot(hist.BinEdges(), pn)
 hold off
