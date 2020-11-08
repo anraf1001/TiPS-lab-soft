28911b37f15625bcaff0d36a03f76bc0d6289a74
Andrzej Rafalski <anraf1001@gmail.com>
2020-11-08 13:00:26 +0100

    Zad03

--- lab02/zad03.m
+++ lab02/zad03.m
@@ -1,11 +1,10 @@
 R = 0.01;
 u = load('u.dat');
-P = mean(abs(u) .^ 2) / R;
+P = mean(u .^ 2) / R;
 
-Pc = zeros(1, 10);
+l10 = length(u) / 10;
+Pc = zeros(1, length(u)-l10);
 
-for i=1:10
-    Pc(i) = mean(abs(u((i - 1)*length(u)/10 + 1:i*length(u)/10)) .^ 2) / R;
+for i=1:length(Pc)
+    Pc(i) = mean(u(i:i+l10-1) .^ 2) / R;
 end
-
-% P = mean(Pc)
