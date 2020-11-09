c8141cbb1905daa273d97eae44ca624bb1f599c1
Andrzej Rafalski <anraf1001@gmail.com>
2020-11-09 19:03:38 +0100

    zad03 - lab02 refactoring

--- lab02/zad03.m
+++ lab02/zad03.m
@@ -3,7 +3,7 @@ u = load('u.dat');
 P = mean(u .^ 2) / R;
 
 l10 = length(u) / 10;
-Pc = zeros(1, length(u)-l10);
+Pc = zeros(1, length(u)-l10 + 1);
 
 for i=1:length(Pc)
     Pc(i) = mean(u(i:i+l10-1) .^ 2) / R;
--- lab02/zad03.py
+++ lab02/zad03.py
@@ -6,7 +6,7 @@ P = np.mean(np.abs(u)**2) / R
 print(P)
 
 l10 = int(len(u) / 10)
-Pc = np.zeros(len(u) - l10)
+Pc = np.zeros(len(u) - l10 + 1)
 
 for i in range(0, len(Pc)):
     Pc[i] = np.mean(np.abs(u[i:i+l10]**2)) / R
